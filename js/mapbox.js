01132a85effd68d3c891be7f1f5de052863f65be
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-16 14:37:16 +0900

    [Update] Initial commit

---
+++ js/mapbox.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken =
+  'pk.eyJ1IjoieW9zaGkxMTI1aGlzYSIsImEiOiJjanRiMHJ6anMwZ3AwM3lyeWZxZmEyaXNyIn0.1C3F-3mSSnGTj42klfVUTw';
+  
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
